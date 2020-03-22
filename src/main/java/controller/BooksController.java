952a73f7d8c2e3fb334470d2d95810340a825acf
Marian-N <mariannisky@gmail.com>
2020-03-23 00:04:13 +0100

    Add books

--- src/main/java/controller/BooksController.java
+++ src/main/java/controller/BooksController.java
@@ -1,4 +1,45 @@
 package controller;
 
+import database.Database;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import model.Book;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class BooksController {
+    private static BooksController _instance = null;
+    private Database database;
+    private Connection connection;
+    private ObservableList<Book> books = FXCollections.observableArrayList();
+
+    public BooksController() throws SQLException, ClassNotFoundException {
+        database = Database.get_instance();
+        connection = Database.getConnection();
+        String query = "SELECT * from books";
+        ResultSet resultSet = connection.createStatement().executeQuery(query);
+        while(resultSet.next()){
+            int id = resultSet.getInt("id");
+            String title = resultSet.getString("title");
+            double price = resultSet.getDouble("price");
+            int stockQuantity = resultSet.getInt("stock_quantity");
+            Date publicationDate = resultSet.getDate("publication_date");
+            int publisherID = resultSet.getInt("publisher_id");
+            String description = resultSet.getString("description");
+            Book book = new Book(id, title, price, stockQuantity, publicationDate, publisherID, description);
+            books.add(book);
+        }
+    }
+
+    public static BooksController getInstance() throws SQLException, ClassNotFoundException {
+        if(_instance == null)
+            _instance = new BooksController();
+        return _instance;
+    }
+
+    public ObservableList<Book> getBooks() {
+        return books;
+    }
 }
