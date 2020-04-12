2cbfb85625231d3332c20e7be7bf9e41e5f425d8
Marian-N <mariannisky@gmail.com>
2020-04-12 19:23:43 +0200

    Add option to create new order

--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -3,9 +3,7 @@ package model;
 import database.Database;
 import javafx.collections.ObservableList;
 
-import java.sql.Date;
-import java.sql.SQLException;
-import java.util.List;
+import java.sql.*;
 
 public class Order {
     private int ID;
@@ -22,6 +20,40 @@ public class Order {
         this.price = price;
         this.status = status;
         this.orderContents = orderContents;
+    }
+
+    public Order(Customer customer, ObservableList orderContents) throws SQLException, ClassNotFoundException {
+        this.customer = customer;
+        this.orderContents = orderContents;
+        this.date = new Date(System.currentTimeMillis());
+        this.price = getTotalPrice();
+        this.status = "nevybavená";
+        String query = "INSERT INTO orders " +
+                "(date, customer_id, price, status)" +
+                " VALUES (?, ?, ?, ?)";
+        Connection connection = Database.getInstance().getConnection();
+        PreparedStatement statement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        statement.setDate(1, date);
+        statement.setInt(2, customer.getID());
+        statement.setDouble(3, price);
+        statement.setString(4, status);
+        this.ID = statement.executeUpdate();
+        statement.close();
+        addToOrderBook();
+    }
+
+    private void addToOrderBook() throws SQLException, ClassNotFoundException {
+        String query = "INSERT INTO order_book (order_id, book_id, quantity) VALUES (?, ?, ?)";
+        Connection connection = Database.getInstance().getConnection();
+        PreparedStatement statement = connection.prepareStatement(query);
+        for(OrderContent orderContent:orderContents) {
+            statement.setInt(1, this.ID);
+            statement.setInt(2, orderContent.getBook().getID());
+            statement.setInt(3, orderContent.getQuantity());
+            statement.addBatch();
+        }
+        statement.executeBatch();
+        statement.close();
     }
 
     public int getID() {
@@ -46,6 +78,13 @@ public class Order {
 
     public void setCustomer(Customer customer) {
         this.customer = customer;
+    }
+
+    private double getTotalPrice() {
+        double total = 0;
+        for(OrderContent orderContent:orderContents)
+            total += orderContent.getBook().getPrice() * orderContent.getQuantity();
+        return total;
     }
 
     public double getPrice() {
