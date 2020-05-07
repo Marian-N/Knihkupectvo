8a02c2db81f087d5bbd029c5caab1c250a1b3c40
Marian-N <mariannisky@gmail.com>
2020-05-07 19:07:29 +0200

    Add function to find authors by name

--- src/main/java/controller/AuthorsController.java
+++ src/main/java/controller/AuthorsController.java
@@ -4,10 +4,19 @@ import database.Database;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableMap;
 import model.Author;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 public class AuthorsController {
     private static AuthorsController _instance = null;
@@ -36,6 +45,18 @@ public class AuthorsController {
     }
 
     public ObservableMap<Integer, Author> getAuthors() {
+        return authors;
+    }
+
+    public List<Author> getAuthor(String name) {
+        Session session = Database.getSessionFactory().openSession();
+        CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+        CriteriaQuery<Author> query = criteriaBuilder.createQuery(Author.class);
+        Root<Author> rootEntry = query.from(Author.class);
+        query.select(rootEntry);
+        query.where(criteriaBuilder.equal(rootEntry.get("name"), name));
+        EntityManager entityManager = session.getEntityManagerFactory().createEntityManager();
+        List<Author> authors = entityManager.createQuery(query).getResultList();
         return authors;
     }
 }
