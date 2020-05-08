82e52baf32a119c285c9c6fc686e66ba9b12db74
Marian-N <mariannisky@gmail.com>
2020-05-08 20:52:49 +0200

    Change login verify to return customer

--- src/main/java/security/Login.java
+++ src/main/java/security/Login.java
@@ -7,15 +7,17 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import java.sql.SQLException;
 
 public class Login {
-    public static boolean verify(String mail, String password) throws SQLException, ClassNotFoundException {
+    public static Customer verify(String mail, String password) throws SQLException, ClassNotFoundException {
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         Customer customer;
         try{
             customer = CustomerController.getInstance().getCustomer(mail);
         }
         catch(Exception e) {
-            return false;
+            return null;
         }
-        return passwordEncoder.matches(password, customer.getEncryptedPassword());
+        if(passwordEncoder.matches(password, customer.getEncryptedPassword()))
+            return customer;
+        return null;
     }
 }
